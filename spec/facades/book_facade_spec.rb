27eb7ba6e4a84706ea77c76a9f5720ae9295f37f
Odell McFarland IV <odell.mac4@gmail.com>
2024-07-25 11:35:15 -0700

    Feat: Create a book poro from response using a facade

--- spec/facades/book_facade_spec.rb
+++ spec/facades/book_facade_spec.rb
@@ -3,11 +3,21 @@ require 'rails_helper'
 RSpec.describe BookFacade, :vcr do
   before(:each) do
     @facade = BookFacade.book_search("freedom writers")
+    @book = BookFacade.book_by_id("AbyoDwAAQBAJ")
     
   end
 
   it 'creates book poros from response' do
-    expect(@facade).to be_an Array
-    expect(@facade.first).to be_a Book
+    VCR.use_cassette("Book_facade/index_page", record: :new_episodes) do
+      expect(@facade).to be_an Array
+      expect(@facade.first).to be_a Book
+    end
   end
+
+  it 'creates a book poro with an id' do
+    VCR.use_cassette("Book_facade/show_page", record: :new_episodes) do
+      expect(@book).to be_a Book
+    end
+  end
+
 end
