cf5b321fb3aa42295a6cff35e87e2f52863a20e1
Odell McFarland IV <odell.mac4@gmail.com>
2024-07-25 11:31:18 -0700

    Feat: extract data from response and reformat description if html included in string

--- app/poros/book.rb
+++ app/poros/book.rb
@@ -18,10 +18,18 @@ class Book
     @authors = book_data[:volumeInfo][:authors]
     @publisher = book_data[:volumeInfo][:publisher]
     @published_date = book_data[:volumeInfo][:publishedDate]
-    @description = book_data[:volumeInfo][:description]
+    @description = format_description(book_data[:volumeInfo][:description])
     @categories = book_data[:volumeInfo][:categories]
     @avg_rating = book_data[:volumeInfo][:averageRating]
     @image_links = book_data[:volumeInfo][:imageLinks]
     @buy_link = book_data[:saleInfo][:buyLink]
+  end
+
+  def format_description(description_data)
+    if description_data.include?("<") || description_data.include?(">") || description_data.include?("/")
+      Nokogiri::HTML(description_data).text.strip
+    else
+      description_data
+    end
   end
 end
