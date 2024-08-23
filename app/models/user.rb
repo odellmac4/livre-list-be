4d07351edb66da7c845f7fe8770de7a05b7c118a
Odell McFarland IV <odell.mac4@gmail.com>
2024-08-23 10:45:36 -0700

    disable oauth not ready for deployment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
 
-  def self.from_omniauth(auth)
-    User.find_or_create_by(uid: auth[:uid], provider: response[:provider]) do |u|
-      u.username = auth[:info][:name]
-      u.email = auth[:info][:email]
-    end
-  end
+  # def self.from_omniauth(auth)
+  #   User.find_or_create_by(uid: auth[:uid], provider: response[:provider]) do |u|
+  #     u.username = auth[:info][:name]
+  #     u.email = auth[:info][:email]
+  #   end
+  # end
 end
